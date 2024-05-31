df7fc17f9a9cba61c7e06ffae3b987626f5bc9f2
cricgok <rgokul.aids2021@citchennai.net>
2024-05-31 12:50:45 +0530

    admin

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -59,7 +59,7 @@ const Cart = () => {
                 <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button onClick={() => navigate('/myorder')}>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
